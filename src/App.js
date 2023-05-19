9c5af914b5406cf4eab70a2fb75c6718c2b94a26
Bilal-Siddiqe <dev.bilalsiddique@gmail.com>
2023-05-20 00:29:13 +0500

    Event Handler

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 import Products from "./components/Products";
 
-
 function App() {
+
   return (
     <div className="App container">
-
       <Products />
-
     </div>
   );
 }
-
 export default App;
+
+
+
+  // let data = { Name: "", Age: 0 }
+  // const [inputData, setInputData] = useState(data)
+
+
+  // function handleChange(e) {
+  //   setInputData({ ...inputData, [e.target.name]: e.target.value })
+  // }
+
+  // function Func(e) {
+  //   e.preventDefault();
+  //   console.log(inputData);
+  //   axios.post("https://jsonplaceholder.typicode.com/users", inputData).then((item) => { console.log(item) })
+  // }
+
+
+// Name :<input id="input" name="Name" value={inputData.Name} onChange={handleChange} />
+// Age : <input id="input" name="Age" value={inputData.Age} onChange={handleChange} />
+// <button onClick={Func}> Submit </button>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,43 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 
-function Products() {
+function Products(props) {
 
     const [data, setData] = useState([]);
-    
+
     useEffect(() => {
-
-        let url = "https://dummyjson.com/products";
-        axios.get(url)
-            .then((item) => {
-                console.log(item.data.products);
-                setData(item.data.products)
-            })
-            .catch((err) => {
-                console.log("404 Error");
-            })
-
+        let url = "https://api.github.com/users";
+        axios.get(url).then(resp).catch(error);
     }, [])
+
+    const resp = (item) => {
+        console.log(item.data);
+        setData(item.data)
+    }
+
+    const error = (err) => {
+        console.log("404 Error");
+    }
+
+    let Login = prompt("Enter Login");
+    let ID = prompt("Enter ID");
+
+    let UserFound = data.find(obj => obj.login == Login && obj.id == ID)
 
 
     return (
         <div className='row container'>
 
-            {
-                data.map((abc) => {
 
-                    return (
-                        <div className="card col-4 mx-2 my-3" style={{ width: "14rem" }}>
-                            <img src={abc.thumbnail} className="card-img-top" alt="..." /> 
-                            <div className="card-body">
-                                <h5 className="card-title">{abc.title}</h5>
-                                <p className="card-text">{abc.description}</p>
-                                <a href="#" className="btn btn-primary">{abc.price}</a>
-                            </div>
+
+            {
+                UserFound ?
+                    <div className="card col-4 mx-2 my-3" style={{ width: "14rem" }}>
+                        <img src={UserFound.avatar_url} className="card-img-top" alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{UserFound.login}</h5>
+                            <p className="card-text">{UserFound.node_id}</p>
+                            <a href="#" className="btn btn-primary">{UserFound.id}</a>
                         </div>
-                    )
-                })
+                    </div>
+                    :
+                    <h1>Invalid </h1>
             }
+
+
+
+
 
         </div>
     )
